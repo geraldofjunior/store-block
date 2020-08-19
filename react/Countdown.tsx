ebd63d8dfcc82f40c668021d01e6d90f44c4da0a
Geraldo <geraldofjunior@gmail.com>
2020-08-18 22:22:13 -0300

    Push inicial

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TimeSplit } from './typings/global';
+import { tick } from './utils/time';
 
-interface CountdownProps { }
+interface CountdownProps {
+    targetDate: string;
+}
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
+const DEFAULT_TARGET_DATE = (new Date('2020-08-20')).toISOString();
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+
+    const [timeRemaining, setTime] = useState<TimeSplit>({
+        hours:   '0',
+        minutes: '0',
+        seconds: '0'
+    });
+
+    tick(targetDate, setTime);
+
     return (
         <div>
-            <h1>Teste Countdown</h1>
+            <h1>{ `${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}` }</h1>
         </div>
     );
 }
@@ -14,7 +29,14 @@ Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+        targetDate: {
+            title: "Data Final",
+            description: "Data final utilizada pelo contador",
+            type: "string",
+            default: "null",
+        },
+    },
 }
 
 export default Countdown
